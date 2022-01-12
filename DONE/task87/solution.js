a0abcc261dbfc773460cdda92b0a7ebd171c7903
iprankurpandey <pprankur@gmail.com>
2022-01-12 21:05:36 +0530

    solved

---
+++ DONE/task87/solution.js
@@ -0,0 +1,11 @@
+/** 
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+
+const name = 'John';
+const age = 30;
+const isMarried = true;
+const city = 'New York';
+const country = 'USA';
